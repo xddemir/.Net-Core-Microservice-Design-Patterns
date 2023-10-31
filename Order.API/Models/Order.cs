dc81aa83b4b58411ee0eadd725c76402b0c7c720
Doğukan Demir <doguka.demir991@gmail.com>
2023-10-31 20:05:08 +0300

    Db Migration completed

---
+++ Order.API/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace Order.API.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    
+    public DateTime CreateDate { get; set; }
+
+    public string BuyerId { get; set; }
+
+    public Address Address { get; set; }
+
+    public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+    public OrderStatus Status { get; set; } = OrderStatus.None;
+
+    public string FailMessage { get; set; }
+}
+
+public enum OrderStatus
+{
+    Suspend,
+    Success,
+    Fail,
+    None
+}
--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Order.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
+{
+    optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+});
 
 var app = builder.Build();
 
